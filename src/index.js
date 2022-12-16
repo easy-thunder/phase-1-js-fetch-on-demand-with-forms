a718049152fb2cf73cf7dc0f0038a1939ecf5476
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-15 17:17:53 -0700

    commit tree a4fe4e1d14a86f854b433f4cdc704ee730bf7d68

--- src/index.js
+++ src/index.js
@@ -1,4 +1,52 @@
 const init = () => {
+
+const inputForm = document.querySelector("form");
+
+inputForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const input = document.querySelector('input#searchByID');
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(r => r.json())
+    .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        summary.textContent = data.summary;
+        title.textContent = data.title;
+    }
+    )
+    
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 }
 
